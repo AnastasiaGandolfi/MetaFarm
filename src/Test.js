b87967b10c36fb4fb9b36562a10bf071c484a426
Katilina88 <caterinalograsso@gmail.com>
2023-04-04 13:23:23 +0200

    feat: add 10-button-today

--- src/Test.js
+++ src/Test.js
@@ -6,7 +6,15 @@ export function Test() {
     <div>
       {/* <Typography />
       <Palette /> */}
-      <Button type="rectangle" text="Visualizza tutto"/>
+      <Button type="rectangle" text="Visualizza tutto" />
+      <br />
+      <Button type="pill-one" text="Oggi" />
+      <br />
+      <Button type="pill-one" text="Ultimi 7 giorni" />
+      <br />
+      <Button type="pill-one" text="Ultimi 30 giorni" />
+    
+      
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,23 @@
-import '../assets/style/css/button.css'
+import { useState } from 'react';
+import '../assets/style/css/button.css';
 
-export function Button({ type , text}) {
+export function Button({ type, text }) {
+  const [style, setStyle] = useState({});
+
+  const handleClick = () => {
+    if (type === 'pill-one') {
+      setStyle((style) => ({
+        backgroundColor: style.backgroundColor === '#dbf0ec' ? '#239c82' : '#dbf0ec',
+        color: style.backgroundColor === '#dbf0ec' ? 'white' : '#239c82'
+      }));
+    }
+  };
+
   return (
     <div>
-      <button className={type}>{text}</button>
+      <button className={type} style={style} onClick={handleClick}>
+        {text}
+      </button>
     </div>
-  )
+  );
 }
-
-
