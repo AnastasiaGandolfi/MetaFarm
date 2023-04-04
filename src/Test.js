5f271c49b3ca8ab7d652bf902f0d3a7ed64ebe7e
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-04-04 14:56:10 +0200

    7-fix: comment test.js

--- src/Test.js
+++ src/Test.js
@@ -1,13 +1,13 @@
 /* import Typography from "./components/Typography";
 import {Palette} from "./components/Palette"; */
-import { Button } from "./components/Button";
-import { BsCalendarWeekFill } from "react-icons/bs"
+/* import { Button } from "./components/Button";
+import { BsCalendarWeekFill } from "react-icons/bs" */
 export function Test() {
   return (
     <div>
       {/* <Typography />
       <Palette /> */}
-      <Button type="rectangle" text="See all" />
+      {/* <Button type="rectangle" text="See all" />
       <br />
       <Button type="pill-one" text="Today" />
       <br />
@@ -15,7 +15,7 @@ export function Test() {
       <br />
       <Button type="pill-one" text="Last 30 Days" />
       <br />
-      <Button type="pill-icon" text={<span><BsCalendarWeekFill/> Upcoming</span>} />
+      <Button type="pill-icon" text={<span><BsCalendarWeekFill/> Upcoming</span>} /> */}
     </div>
   );
 }
