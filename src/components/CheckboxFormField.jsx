84ee7495e668faee3b478ad4872e822ab1e74016
Simone Checco <simone.checco1995@gmail.com>
2023-04-04 16:28:32 +0200

    feat: add 13-checkbox-form-field

---
+++ src/components/CheckboxFormField.jsx
@@ -0,0 +1,27 @@
+import "../assets/style/css/checkboxformfield.css";
+import "../assets/style/css/typography.css";
+import { ReactComponent as Checkbox } from "../assets/icons/svgexport-14.svg";
+import { ReactComponent as Checkedbox } from "../assets/icons/svgexport-15.svg";
+import { useState } from "react";
+export function CheckboxFormField() {
+  const [check, setCheck] = useState(false);
+  function handleClick() {
+    setCheck((flag) => !flag);
+  }
+  return (
+    <div className="checkbox-container ">
+      <div className="checkbox">
+        {check === false ? (
+          <Checkbox onClick={handleClick} />
+        ) : (
+          <Checkedbox onClick={handleClick} />
+        )}
+      </div>
+      <div className="typography-container">
+        <p className="text-small">
+          Digitando la mia mail e iscrivendomi confermo e accetto quanto sopra
+        </p>
+      </div>
+    </div>
+  );
+}
