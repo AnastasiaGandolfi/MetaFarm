e0c2f019c2ea7c43c4d84ab4b185492fc0576368
Katilina88 <caterinalograsso@gmail.com>
2023-04-06 15:57:53 +0200

    feat: add 29-card-best-collection-item

--- src/Root.tsx
+++ src/Root.tsx
@@ -8,7 +8,11 @@ import { Button } from "./components/Button";
 import { BsCalendarWeekFill } from "react-icons/bs" 
 import { CheckboxFormField } from "./components/CheckboxFormField";
 import { FormField } from "./components/FormField";
+import { Card } from "./components/Card"
+import esempioImage from "../src/assets/images/esempio-image-card.avif";
+import smallImage from "../src/assets/images/small-image.avif"; 
 import Nav from 'react-bootstrap/Nav';
+
 
 const ComponentElements:{element:JSX.Element,title:string}[] = [
   {element:<Typography/>, title:"Typography"},
@@ -19,7 +23,8 @@ const ComponentElements:{element:JSX.Element,title:string}[] = [
   {element:<Button type="pill-one" text="Last 30 Days"/>, title:"Button"},
   {element:<Button type="pill-icon" text={<span><BsCalendarWeekFill/> Upcoming</span>}/>, title:"Button"},
   {element:<CheckboxFormField/>, title:"CheckboxFormField"},
-  {element:<FormField/>, title:"FormField"}
+  {element:<FormField/>, title:"FormField"},
+  {element: <Card image={esempioImage} smallImage={smallImage} mainTitle="Loaded Lions" subtitle="Loaded Lion #289" body="1 minted" paragraph="Last Sale" price="3,456" venditeTotali="Total Sales: $4.2K" />, title:"Card-Best-Collection-item"}
 ]
 
 export function Root() {
