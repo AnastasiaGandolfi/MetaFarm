504fdf0300b1ad1d49ec699a007cd43713123834
georgev97 <georgeandrei.varga@gmail.com>
2023-04-11 11:03:13 +0200

    feat: fix merge 19

--- src/Root.tsx
+++ src/Root.tsx
@@ -19,7 +19,7 @@ import { BrandCarousel } from "./components/BrandCarousel";
 import { CardCollection } from "./components/CardCollection";
 import CardBestAuthor from "./components/CardBestAuthor";
 import { icons } from "react-icons/lib";
-import {CartModal} from "./components/CartModal";
+import { CartModal } from "./components/CartModal";
 import SwitchButton from "./components/SwitchButton";
 import { NavbarDropdownButton } from "./components/NavbarDropdownButton";
 
@@ -81,11 +81,6 @@ const ComponentElements: { element: JSX.Element; title: string }[] = [
   },
   {
     element: (
-      <CardCollection image={esempioImage} smallImage={smallImage} paragraph="Otherdeed for Otherside" />)
-  , title:"Card-Collection"
-},
-{ element: <CardBestAuthor paragraph=" $2,000,000+ " />,title:"card-best-author",   },
-{ element: <CartModal />,title:"Cart Modal",   },
       <CardCollection
         image={esempioImage}
         smallImage={smallImage}
@@ -98,6 +93,7 @@ const ComponentElements: { element: JSX.Element; title: string }[] = [
     element: <CardBestAuthor paragraph=" $2,000,000+ " />,
     title: "card-best-author",
   },
+  { element: <CartModal />, title: "Cart Modal" },
   {
     element: (
       <NavbarDropdownButton
