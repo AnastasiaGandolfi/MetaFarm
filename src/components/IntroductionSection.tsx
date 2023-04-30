1830bd484e406981d017ad63fca4682d5da228ac
Valentyna Hnatyuk <hnatyukvalentyna01@gmail.com>
2023-04-30 18:19:05 +0200

    introductionsection logo animation added

--- src/components/IntroductionSection.tsx
+++ src/components/IntroductionSection.tsx
@@ -4,7 +4,7 @@ export function IntroductionSection() {
 
   return (
     <div className="introduction-section">
-      <img src={logoImage} className="logo-image" />
+      <img src={logoImage} className="logo-image animate__animated animate__zoomIn " />
       <h2 className="intro-text">Find out more about the project</h2>
       <button className="intro-button">Check out</button>
     </div>
