20d428f07b4d09dc44df9e949287d6c92153d781
Valentyna Hnatyuk <hnatyukvalentyna01@gmail.com>
2023-04-30 22:54:40 +0200

    page-not-found animations added

--- src/pages/PageNotFoundDue.js
+++ src/pages/PageNotFoundDue.js
@@ -1,31 +1,69 @@
- import React from "react";
- import pompelmoImage from "../assets/img/Pompelmo.png";
- import fourOrangeImage from "../assets/img/fourorange (1).png";
+import React, { useRef, useEffect } from "react";
+import pompelmoImage from "../assets/img/Pompelmo.png";
+import fourOrangeImage from "../assets/img/fourorange (1).png";
 import { Link } from "react-router-dom";
+import "animate.css";
 
 function PageNotFound() {
+  const lineOneRef = useRef(null);
+  const lineTwoRef = useRef(null);
+  const lineThreeRef = useRef(null);
+
+  useEffect(() => {
+    lineThreeRef.current.classList.add("animate");
+    return () => {
+      lineThreeRef.current.classList.remove("animate");
+    };
+  }, [lineThreeRef]);
+
+  const handleLineOneAnimationEnd = () => {
+    lineTwoRef.current.style.opacity = "1";
+    lineTwoRef.current.classList.add("animate");
+    lineTwoRef.current.addEventListener("animationend", () => {
+      lineThreeRef.current.style.opacity = "1";
+      lineThreeRef.current.classList.add("animate");
+    });
+  };
   return (
-      <div className="page-not-found-due">
-        <div className="text-container">
-          <div className="terminal">
-            <h1>Oops! <span className="errorcode">This is a shady page!</span></h1>
-            <p className="output">Pompelmo has taken over. You know what to do...</p>
-            <p className="output"> Go back to our Home Page to avoid the citrusy consequences! <Link to="/home">go back</Link></p>
-            <p className="output">Good luck.</p>
-          </div>
+    <div className="page-not-found-due">
+      <div className="text-container">
+        <div className="terminal">
+          <h1 className="error animate__animated animate__flash">
+            Oops!{" "}
+            <span className="errorcode">
+              This is a shady page!
+            </span>
+          </h1>
+          <p
+            className="output typing-demo line-one"
+            onAnimationEnd={handleLineOneAnimationEnd}
+            ref={lineOneRef}
+          >
+            Pompelmo has taken over. You know what to do...
+          </p>
+          <p
+            className="output typing-demo line-two"
+            ref={lineTwoRef}
+          >
+            {" "}
+            Go back to our Home Page to avoid the citrusy consequences!{" "}
+            <Link to="/home">go back</Link>
+          </p>
+          <p className="output typing-demo line-three" ref={lineThreeRef}>
+            Good luck.
+          </p>
+        </div>
         <div className="image-container">
           <img className="four-image" src={fourOrangeImage} />
           <img className="pompelmo-image" src={pompelmoImage} />
           <img className="four-image" src={fourOrangeImage} />
         </div>
-      <div className="noise"></div>
-      <div className="overlay"></div>
+        <div className="noise"></div>
+        <div className="overlay"></div>
       </div>
-      </div>
-  
+    </div>
   );
 }
-
 
 export default PageNotFound;
 
